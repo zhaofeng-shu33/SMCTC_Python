8899bd2a593bda24919d16a9baad89f35ee68f58
zhaofeng-shu33 <616545598@qq.com>
2018-08-12 16:06:42 +0800

    add basic docstring to smctc_ext

--- smctc_ext.cpp
+++ smctc_ext.cpp
@@ -19,6 +19,11 @@ using namespace boost::python;
 #endif
 #define Sampler smc::sampler_python<list>
 
+const char move_set_constructor_doc_string []=
+"\
+Create a reduced moveset with a single move\n\
+User provides two functions: fInitialise and fMove\
+";
 
 void translate(smc::moveset_python_init_exception const& e)
 {
@@ -27,6 +32,9 @@ void translate(smc::moveset_python_init_exception const& e)
 
 BOOST_PYTHON_MODULE(smctc)
 {
+  //disable the C++ signatures
+  docstring_options local_docstring_options(/*show_user_defined*/ true, 
+  /*show_py_signatures*/ true, /*show_cpp_signatures*/ false);    
     scope().attr("__version__") = __SMC_TDSMC_HH;
 
     enum_<ResampleType>("ResampleType")
@@ -47,18 +55,20 @@ BOOST_PYTHON_MODULE(smctc)
     .def("GetValue", &Particle::GetValue, return_value_policy<copy_const_reference>())
     .def("AddToLogWeight", &Particle::AddToLogWeight);
 
-    class_<Sampler, boost::noncopyable>("sampler", init<long, HistoryType>())
-    .def("SetMoveSet", &Sampler::SetMoveSet)
-    .def("Initialise", &Sampler::Initialise)
-    .def("SetResampleParams", &Sampler::SetResampleParams)
-    .def("Iterate", &Sampler::Iterate)
-    .def("Integrate_Mean", &Sampler::Integrate_Mean);
+    class_<Sampler, boost::noncopyable>("sampler", 
+    "class for an interacting particle system suitable for SMC sampling",
+    init<long, HistoryType>())
+    .def("SetMoveSet", &Sampler::SetMoveSet, "Sets the entire moveset to the one which is supplied")
+    .def("Initialise", &Sampler::Initialise, "Initialise the Sampler and its constituent particles")
+    .def("SetResampleParams", &Sampler::SetResampleParams, "Set Resampling Parameters")
+    .def("Iterate", &Sampler::Iterate, "Perform one iteration of the simulation algorithm")
+    .def("Integrate_Mean", &Sampler::Integrate_Mean, "Get the mean value at dimension (id)");
 
     class_<smc::rng>("rng",init<>())
     .def("Normal", &smc::rng::Normal);
 
     register_exception_translator<smc::moveset_python_init_exception>(&translate);
-    class_<smc::moveset_python>("moveset", init<object, object>())
+    class_<smc::moveset_python>("moveset", move_set_constructor_doc_string, init<object, object>())
     .def(init<>())
     .def("DoMove", &smc::moveset_python::DoMove)
     .def("DoInit", &smc::moveset_python::DoInit, return_value_policy<manage_new_object>());
