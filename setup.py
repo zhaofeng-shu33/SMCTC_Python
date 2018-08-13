91b9aeae885e08907e9667df2983b8fb2bca897c
zhaofeng-shu33 <616545598@qq.com>
2018-08-13 21:26:17 +0800

    remove language python from .travis.yml

--- setup.py
+++ setup.py
@@ -1,9 +1,17 @@
 import os
+import sys
 from setuptools import Extension, setup
+
+if False:
+    osx_specific = '-stdlib=libc++'
+else:
+    osx_specific = ''
+
+boost_py_lib = '-lboost_python' + sys.version[0] + sys.version[2]
 
 module = Extension('smctc', sources=['smctc_ext.cpp'], 
 include_dirs = ['./include'],
-extra_link_args=['-lboost_python', '-L./include', '-lsmctc','-lgslcblas', '-lgsl', '-stdlib=libc++'],
-extra_compile_args=['-O0', '-stdlib=libc++'])
+extra_link_args=[boost_py_lib, '-L./include', '-lsmctc','-lgslcblas', '-lgsl', osx_specific],
+extra_compile_args=['-O0', osx_specific])
 
 setup(name='smctc', ext_modules = [module])
