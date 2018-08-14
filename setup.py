c8f3b0ae1d9462534ec241f5e79515f991a6ccd8
zhaofeng-shu33 <616545598@qq.com>
2018-08-14 09:37:06 +0800

    add ubuntu build

--- setup.py
+++ setup.py
@@ -6,12 +6,15 @@ if False: # if necessay, change False to True
     osx_specific = '-stdlib=libc++'
 else:
     osx_specific = ''
-
-boost_py_lib = '-lboost_python' + sys.version[0] + sys.version[2]
+if sys.platform =='linux2':
+	boost_py_lib = '-lboost_python-py' + sys.version[0] + sys.version[2] 
+else:
+	boost_py_lib = '-lboost_python' + sys.version[0] + sys.version[2]
 
 module = Extension('smctc', sources=['smctc_ext.cpp'], 
 include_dirs = ['./include'],
-extra_link_args=[boost_py_lib, '-L./include', '-lsmctc','-lgslcblas', '-lgsl', osx_specific],
-extra_compile_args=['-O0', osx_specific])
+extra_link_args=[boost_py_lib, '-L./include', '-lsmctc','-lgsl', '-lgslcblas'],
+extra_compile_args=['-O0']
+)
 
 setup(name='smctc', ext_modules = [module])
