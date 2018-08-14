43c8af60ab38544d6e627055c6f633f71bf42625
zhaofeng-shu33 <616545598@qq.com>
2018-08-14 11:46:54 +0800

    add windows support

--- setup.py
+++ setup.py
@@ -1,20 +1,46 @@
 import os
 import sys
 from setuptools import Extension, setup
-
-if False: # if necessay, change False to True
-    osx_specific = '-stdlib=libc++'
+DEBUG = False # if produce debug version, change False to True
+if False: # for osx version<=9, change False to True
+    osx_use_libc = '-stdlib=libc++'
 else:
-    osx_specific = ''
-if sys.platform =='linux2':
-	boost_py_lib = '-lboost_python-py' + sys.version[0] + sys.version[2] 
+    osx_use_libc = ''
+sources_list = ['smctc_ext.cpp']
+link_flag_list = []
+compile_flag_list = []
+include_dir_list = ['./include']
+if sys.platform == 'linux2':
+    boost_py_lib = '-lboost_python-py' + sys.version[0] + sys.version[2] 
+elif sys.platform == 'darwin':
+    boost_py_lib = '-lboost_python' + sys.version[0] + sys.version[2]
+    if(osx_use_libc):
+        link_flag_list.append(osx_use_libc)
+        compile_flag_list.append(osx_use_libc)
+elif sys.platform == 'win32':
+    boost_py_lib = ''
+    sources_list.extend(['./include/history.cc','./include/rng.cc', './include/smc-exception.cc'])
 else:
-	boost_py_lib = '-lboost_python' + sys.version[0] + sys.version[2]
+    raise Exception("Unsupported platform")
 
-module = Extension('smctc', sources=['smctc_ext.cpp'], 
-include_dirs = ['./include'],
-extra_link_args=[boost_py_lib, '-L./include', '-lsmctc','-lgsl', '-lgslcblas'],
-extra_compile_args=['-O0']
+if sys.platform == 'win32':
+    # for windows, we only support condas build
+    conda_include = os.path.join(os.environ['CONDA_PREFIX'],'Library','include')
+    include_dir_list.append(conda_include)
+    if(DEBUG):
+        compile_flag_list.append('/Od')    
+else:
+    # for other platforms, we only support system python build
+    link_flag_list.extend(['-L./include', '-lsmctc','-lgsl', '-lgslcblas'])
+    if(DEBUG):
+        compile_flag_list.append('-O0')    
+    
+link_flag_list.append(boost_py_lib)
+
+module = Extension('smctc', sources=sources_list, 
+include_dirs = include_dir_list,
+extra_link_args = link_flag_list,
+extra_compile_args = compile_flag_list
 )
 
 setup(name='smctc', ext_modules = [module])
